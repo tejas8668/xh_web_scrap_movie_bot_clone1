5f27202ef0fa78cf937c9b9f6176d68816fb9bd5
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-12-27 13:02:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import urllib.parse
 
 # Configure logging
 logging.basicConfig(
-    format='%(asctime)s - %(name=s - %(levelname=s - %(message=s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
