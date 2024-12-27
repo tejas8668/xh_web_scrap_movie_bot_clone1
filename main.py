3c9b6fa26677dfda739ad74825102c2bdf005491
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-12-27 10:02:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,6 @@ logger = logging.getLogger(__name__)
 # Get the bot token from environment variables
 TOKEN = os.getenv('BOT_TOKEN')
 
-# Define the allowed domains
-ALLOWED_DOMAINS = [
-    "www.mirrobox.com", "www.nephobox.com", "freeterabox.com", "www.freeterabox.com",
-    "1024tera.com", "4funbox.co", "www.4funbox.com", "mirrobox.com", "nephobox.com",
-    "terabox.app", "terabox.com", "www.terabox.app", "terabox.fun", "www.terabox.com",
-    "www.1024tera.com", "www.momerybox.com", "teraboxapp.com", "momerybox.com",
-    "tibibox.com", "www.tibibox.com", "www.teraboxapp.com"
-]
-
 # Define the /start command handler
 async def start(update: Update, context: CallbackContext) -> None:
     logger.info("Received /start command")
@@ -32,13 +23,9 @@ async def start(update: Update, context: CallbackContext) -> None:
 async def handle_link(update: Update, context: CallbackContext) -> None:
     logger.info("Received message: %s", update.message.text)
     original_link = update.message.text
-    domain = urllib.parse.urlparse(original_link).netloc
-    if domain in ALLOWED_DOMAINS:
-        parsed_link = urllib.parse.quote(original_link, safe='')
-        modified_link = f"{parsed_link}&m=0"
-        await update.message.reply_text(modified_link)
-    else:
-        await update.message.reply_text("This domain is not supported.")
+    parsed_link = urllib.parse.quote(original_link, safe='')
+    modified_link = f"{parsed_link}&m=0"
+    await update.message.reply_text(modified_link)
 
 def main() -> None:
     # Get the port from the environment variable or use default
